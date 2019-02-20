6dae47b4bda5516023d5ee3c5bbc9d3e15131c3f
Tzikas <niko.tzikas@gmail.com>
2019-02-20 10:33:55 -0500

    roles

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -78,6 +78,6 @@ function isLoggedIn(req, res, next) {
   if (req.isAuthenticated())
       return next();
 
-  res.redirect('/');
+  res.redirect('/login');
 }
 module.exports = authRoutes;
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,27 @@ const express = require('express');
 const router  = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/', checkRoles('ADMIN'), (req, res, next) => {
   console.log('req.session', req.session, 'req.user', req.user)
   res.render('index');
 });
 
 
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+      return next();
+
+  res.redirect('/login');
+}
+
+function checkRoles(role) {
+  return function(req, res, next) {
+    if (req.isAuthenticated() && req.user.role === role) {
+      return next();
+    } else {
+      res.redirect('/login')
+    }
+  }
+}
 
 module.exports = router;
