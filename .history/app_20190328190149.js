b5fc586d85ee0094959d22e1a2dc17b25fc40ffd
Mike <troianello@protonmail.com>
2019-03-28 19:05:13 -0400

    favicon route changed

---
+++ .history/app_20190328190149.js
@@ -0,0 +1,130 @@
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+const flash = require("connect-flash");
+const User = require('./models/user')
+const ensureLogin = require("connect-ensure-login");
+
+const DBPASS = process.env.DBPASS;
+
+hbs.registerHelper('json', function(context) {
+  return JSON.stringify(context);
+});
+
+// app.js
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+// app.js
+// Mongoose configuration
+mongoose.Promise = Promise;
+mongoose
+  .connect(`mongodb+srv://Mike:${DBPASS}@cluster0-icuim.mongodb.net/test?retryWrites=true`)
+  .then(() => {
+    console.log('Connected to Mongo!')
+  }).catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+
+// ...other code
+
+
+
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+
+app.use(require('node-sass-middleware')({
+  src:  path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+      
+// app.js
+app.use(session({
+  secret: "yadayadayada",
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+  //console.log('serialize')
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+    //console.log('unserialize')
+  });
+
+});
+app.use(flash());
+passport.use(new LocalStrategy({
+  passReqToCallback: true
+}, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+// app.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+
+// default value for title local
+app.locals.title = 'Express - Generated with IronGenerator';
+
+// Routes
+const authRoutes = require("./routes/auth-routes");
+app.use('/', authRoutes);
+
+const index = require('./routes/index');
+app.use('/', index);
+
+
+
+
+
+module.exports = app;
+
+
